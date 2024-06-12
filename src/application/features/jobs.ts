14f47314bef87fde077b8f81e80f1f79637d3b2a
Tharindu Dananjaya <dananjayaabeytd@gmail.com>
2024-06-12 13:59:50 +0530

    db connected

--- src/application/features/jobs.ts
+++ src/application/features/jobs.ts
@@ -1,21 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
+import Job from "../../persistance/entities/jobs";
 
 export const getJobs = async (req: Request, res: Response) => {
   try {
-    const jobs = [
-      {
-        _id: "xyz",
-        title: "Intern - Software Engineer",
-        type: "Full-time",
-        location: "Remote",
-      },
-      {
-        _id: "abc",
-        title: "Software Engineer",
-        type: "Full-time",
-        location: "Remote",
-      },
-    ];
+    const jobs = await Job.find();
     return res.status(200).json(jobs);
   } catch (error) {
     return res.status(500).send();
@@ -24,8 +12,21 @@ export const getJobs = async (req: Request, res: Response) => {
 
 export const createJob = async (req: Request, res: Response) => {
   try {
+    const job = req.body;
+    await Job.create(job);
     return res.status(200).send();
   } catch (error) {
+    return res.status(500).send();
+  }
+};
+
+export const getJobById = async (req: Request, res: Response) => {
+  try {
+    const job = Job.findById(req.params.id);
+    return res.status(200).json(job);
+  } catch (error) {
+    console.log(error);
+
     return res.status(500).send();
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import "dotenv/config";
 import express from "express";
 import jobsRouter from "./api/jobs";
+import { connectDB } from "./persistance/db";
+import jobApplicationsRouter from "./api/jobApplications";
 
 const app = express();
 app.use(express.json());
 
+connectDB();
+
 app.use("/jobs", jobsRouter);
+app.use("/jobApplications", jobApplicationsRouter);
 
 app.listen(8000, () => console.log("Server is listening on port 8000."));
