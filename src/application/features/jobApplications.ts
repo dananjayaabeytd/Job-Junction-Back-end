de68926353878903c6417f04f9e834164007ce3f
Tharindu Dananjaya <dananjayaabeytd@gmail.com>
2024-06-12 14:45:46 +0530

    major updates

--- src/application/features/jobApplications.ts
+++ src/application/features/jobApplications.ts
@@ -1,21 +1,54 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import JobApplication from "../../persistance/entities/JobApplication";
+import NotFoundError from "../../domain/errors/not-found-error";
 
-export const createJobApplication = async (req: Request, res: Response) => {
+export const createJobApplication = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const jobApplication = req.body;
     await JobApplication.create(jobApplication);
-    return res.status(200).send();
+    return res.status(201).send();
   } catch (error) {
-    return res.status(500).send();
+    next(error);
   }
 };
 
-export const getJobApplications = async (req: Request, res: Response) => {
+export const getJobApplications = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const jobApplications = await JobApplication.find().populate("job");
+    const { jobId } = req.query;
+    if (!jobId) {
+      const jobApplications = await JobApplication.find()
+        .populate("job")
+        .exec();
+      return res.status(200).json(jobApplications);
+    }
+    const jobApplications = await JobApplication.find({ job: jobId });
     return res.status(200).json(jobApplications);
   } catch (error) {
-    return res.status(500).send();
+    next(error);
+  }
+};
+
+export const getJobApplicationById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const jobApplication = await JobApplication.findById(id);
+    if (jobApplication === null) {
+      throw new NotFoundError("Job Application not found");
+    }
+    return res.status(200).json(jobApplication);
+  } catch (error) {
+    next(error);
   }
 };
