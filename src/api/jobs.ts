10fa723637843a57f4c3843757c1d6486a5d1354
Tharindu Dananjaya <dananjayaabeytd@gmail.com>
2024-06-15 08:03:22 +0530

    auth middleware added

--- src/api/jobs.ts
+++ src/api/jobs.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { createJob, getJobById, getJobs } from "../application/features/jobs";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 const jobsRouter = express.Router();
 
-jobsRouter.route("/").post(createJob).get(getJobs);
-jobsRouter.route("/:id").get(getJobById);
+jobsRouter.route("/").post(ClerkExpressRequireAuth({}),createJob).get(getJobs);
+jobsRouter.route("/:id").get(ClerkExpressRequireAuth({}),getJobById);
 
 export default jobsRouter;
